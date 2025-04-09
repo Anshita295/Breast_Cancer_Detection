059faa437f6774e9a9c41f66a30885d79a50faaa
Anshita Agrawal <125623667+Anshita295@users.noreply.github.com>
2025-04-09 08:41:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import joblib
 import sklearn.datasets
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
